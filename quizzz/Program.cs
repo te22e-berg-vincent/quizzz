93849e995c95dd561ea983f0dcb1e665a8b97e53
Vincent Berg <vincent.berg@elev.ga.ntig.se>
2023-09-19 10:29:34 +0200

    commit (quiz)

---
+++ quizzz/Program.cs
@@ -0,0 +1,32 @@
+﻿string svar = "";
+svar = svar.ToLower();
+
+int sum = 0;
+
+Console.WriteLine("1) In what region i Japan");
+Console.WriteLine("a) Europe b Africa c Asia");
+svar = Console.ReadLine();
+svar = svar.ToLower();
+
+if (svar == "c")
+{
+Console.WriteLine("Correct");
+sum++;
+}
+
+
+
+
+else if (svar != "c" )
+{
+    Console.WriteLine("Incorrect");
+}
+
+Console.ReadLine();
+Console.WriteLine("1) In what region i Japan");
+Console.WriteLine("a) Europe b Africa c Asia");
+
+
+
+svar = Console.ReadLine();
+
